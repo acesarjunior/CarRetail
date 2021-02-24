8f59af9727a505cc1f61e9a40eae652737e5049e
acesarjunior <42580084+acesarjunior@users.noreply.github.com>
2021-02-24 14:54:51 +0100

    Delete app.ts

--- app.ts
+++
@@ -1,8 +0,0 @@
-let mensagem: string = "Seja Bem Vindo"
-let temporadasFriends: number = 10
-let estudarAngular: boolean = true
-let listaDeFrutas: Array<string> = ['Uva','Banana','Abacaxi']
-let notasDasProvas: Array<number> = [7.5, 8, 9]
-
-mensagem = 'Oi'
-console.log(mensagem)
